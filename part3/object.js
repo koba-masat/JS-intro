bce22436c2ae9245e75a2485cd8307eac5b79676
koba-masat <masatennis10140409@gmail.com>
2022-03-24 08:35:55 +0900

    add obj->toString,valueOf

---
+++ part3/object.js
@@ -0,0 +1,19 @@
+var obj = new Object();
+console.log(obj.toString());
+console.log(obj.valueOf());
+
+var dat = new Date();
+console.log(dat.toString());
+console.log(dat.valueOf());
+
+var arr = ["a", "b", "c"];
+console.log(arr.toString());
+console.log(arr.valueOf());
+
+var num = 10;
+console.log(num.toString());
+console.log(num.valueOf());
+
+var regexp = /[0-9]*[a-z]/gi
+console.log(regexp.toString());
+console.log(regexp.valueOf());
