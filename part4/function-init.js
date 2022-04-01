fe8d36a3abbf29d02a13a5ab1fd23467edb52bd8
koba-masat <masatennis10140409@gmail.com>
2022-04-01 15:53:45 +0900

    functionの定義タイミング

---
+++ part4/function-init.js
@@ -0,0 +1,10 @@
+getTriangle(2, 4);
+getSquare(2, 4);
+
+function getTriangle(base, height) {
+    return base * height / 2;
+}
+
+var getSquare = function (base, height) {
+    return base * height;
+}
