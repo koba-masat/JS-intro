5b9f9a5912d4431cecc678a5f535f12e3942a948
koba-masat <masatennis10140409@gmail.com>
2022-04-01 15:57:40 +0900

    scope

---
+++ part4/scope.js
@@ -0,0 +1,15 @@
+var a = 1;
+function change() {
+    var a = 2;
+    console.log(a);
+}
+change()
+console.log(a);
+
+b = 1;
+function change2() {
+    b = 2;
+    console.log(b);
+}
+change2();
+console.log(b);
