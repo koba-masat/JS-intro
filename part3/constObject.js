f0dbc64159f8021b73fc108855031ce00553384d
koba-masat <masatennis10140409@gmail.com>
2022-04-01 15:06:03 +0900

    object->make const!

--- part3/constObject.js
+++ part3/constObject.js
@@ -1,24 +1,10 @@
-var pet_prev = { type: "hamu", name: "pochi" };
-var pet_seal = { type: "hamu", name: "pochi" };
-var pet_freeze = { type: "hamu", name: "pochi" };
+'use strict';
+var pet = { type: "hamu", name: "pochi" };
 
-var changeProps = (object)=> {
-    object.name = "hachi";
-    delete object.type;
-    object.weight = 42;
-};
+Object.preventExtensions(pet);
+// Object.seal(pet)
+// Object.freeze(pet)
 
-Object.preventExtensions(pet_prev);
-Object.seal(pet_seal)
-Object.freeze(pet_freeze)
-
-changeProps(pet_prev)
-changeProps(pet_seal)
-changeProps(pet_freeze)
-
-console.log("preventExtensions = ");
-console.log(pet_prev);
-console.log("seal = ");
-console.log(pet_seal);
-console.log("freeze = ");
-console.log(pet_freeze);
+pet.name = "hachi";
+delete pet.type;
+pet.weight = 42;
