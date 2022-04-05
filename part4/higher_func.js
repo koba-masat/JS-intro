e5ac77e7097f17ce5411d01643ced318ae35c728
koba-masat <masatennis10140409@gmail.com>
2022-04-06 08:40:53 +0900

    高階関数の使用方法を増やした

--- part4/higher_func.js
+++ part4/higher_func.js
@@ -7,9 +7,20 @@ function higher(dict, func) {
 function showDictElems(key, value) {
     console.log('key=' + key + ', value=' + value);
 }
+
 let dict = {
     type: 'dog',
     name: 'pochi'
 };
 
 higher(dict, showDictElems);
+higher(dict, (key, value) => {
+    console.log('key=' + key + ', value=' + value);
+});
+
+let result = 0;
+let arr = [1, 3, 5, 7, 9];
+higher(arr, (key, value) => {
+    result += value;
+});
+console.log(result);
